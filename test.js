f7609113cd0377a90aa5a6210eeffe291f5c14e9
JheevaShankar <jheeva2005@gamil.com>
2025-10-14 15:34:14 +0530

    Preview of all one i have done

--- test.js
+++ test.js
@@ -45,6 +45,23 @@
 //     .then(response => response.json())
 //     .then(data => console.log(data))
 //     .catch(error => console.error('Error:', error));
-let name='Geevi'
-let age=21;
-console.log('')
+// let name='Geevi'
+// let age=21;
+// console.log('my name is '+name+' and my age is '+age+'');
+// const mul=(a,b)=>{
+//     return a*b;
+// }
+// const square=(n)=>{
+//     return mul(n,n);
+// }
+// const print=(n)=>{
+//     let s=square(n);
+//     console.log(s);
+// }
+// print(5);
+console.log('first');
+Promise.resolve().then(console.log('From Promise'));
+setTimeout(()=>{
+    console.log('Inside Timeout');
+},2000);
+console.log('Last');
